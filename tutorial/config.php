09ef949872010f131b257e9cd8bc85dc17682a9a
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-01 18:09:40 -0700

    Add database connection

--- tutorial/config.php
+++ tutorial/config.php
@@ -1,2 +1,20 @@
 <?php
 $base_url = "http://localhost/tutorial/";
+
+function connection() {
+    $host = 'localhost';
+    $user = 'eq';
+    $password = 'root';
+    $database = 'tutorial';
+    $conn = mysqli_connect($host, $user, $password, $database);
+    
+    if (!$conn) {
+        return FALSE;
+    }
+    
+    return $conn;
+}
+
+function disconnect() {
+    mysqli_close($conn);
+}
