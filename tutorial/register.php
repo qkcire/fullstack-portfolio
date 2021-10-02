527327737a78a624ae12efb7ced62bffbcc368a6
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-01 18:10:17 -0700

    Add registration feature

--- tutorial/register.php
+++ tutorial/register.php
@@ -1,7 +1,32 @@
 <?php
 
+require 'config.php';
 include 'layout/header.php';
 
-echo "In registration page";
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+    
+    $conn = connection();
+    echo md5($password);
+    $sql = "INSERT INTO `users` ('id', 'email', 'mobile', 'password', 'forgot_password_code') VALUES('', '$email', '$mobile', '$password', 'null')";
+    echo $sql;
+    exit();
+}
+
+?>
+
+<html>
+    <body>
+        <form action="" method="POST">
+            Enter your email: <input type="email" name="email"><br>
+            Enter your mobile #: <input type="number" name="mobile"><br>
+            Enter your password: <input type="password" name="password"><br>
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
+<?php
 
 include 'layout/footer.php';
